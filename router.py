7fae9d7cb1f142ff4f685212a9c2c53271169000
sunwu <why@gmail.com>
2023-05-08 19:44:05 +0900

    no secret

--- router.py
+++ router.py
@@ -17,7 +17,7 @@ from services import login_service
 
 router = APIRouter()
 
-cred = credentials.Certificate("/app/gcp.json")
+cred = credentials.Certificate("file-ground-firebase-adminsdk-k8zwz-2dbb250413.json")
 firebase_admin.initialize_app(cred,
                               {'databaseURL':'https://file-ground-default-rtdb.asia-southeast1.firebasedatabase.app//'})
 
